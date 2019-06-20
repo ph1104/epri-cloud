1ddf648e395c19755ae7f5f0a687e4e7c654e8cd
penghui <1751644135@qq.com>
2019-06-20 15:15:08 +0800

    pom

---
+++ epri-cloud-rabc/src/main/java/com/ph/epri/rabc/config/SwaggerConfig.java
@@ -0,0 +1,14 @@
+package com.ph.epri.rabc.config;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author penghui
+ * @date 2019/6/20 0020 9:16
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+}
