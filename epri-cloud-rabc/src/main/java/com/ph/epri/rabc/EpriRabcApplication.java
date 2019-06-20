74d75deff4b74373607a276fd30c0438ba6709ba
penghui <1751644135@qq.com>
2019-06-20 17:08:06 +0800

    pom

--- epri-cloud-rabc/src/main/java/com/ph/epri/rabc/EpriRabcApplication.java
+++ epri-cloud-rabc/src/main/java/com/ph/epri/rabc/EpriRabcApplication.java
@@ -4,7 +4,6 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author penghui
@@ -13,8 +12,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @MapperScan("com.ph.epri.rabc.dao")
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableSwagger2
-//@EnableFeignClients
 public class EpriRabcApplication {
     public static void main(String[] args) {
         SpringApplication.run(EpriRabcApplication.class,args);
